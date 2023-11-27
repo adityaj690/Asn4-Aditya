3d6b70c086cc18b565dd3d0c7e5d7b476f6bc873
adityaj690 <adityaj690@gmail.com>
2023-11-26 22:46:03 -0500

    port changes

--- app.js
+++ app.js
@@ -210,12 +210,10 @@ app.delete('/api/invoices/:id', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
-
+// const PORT = process.env.PORT || 7000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
 
 app.listen(port);
 console.log("App listening on port : " + port);
